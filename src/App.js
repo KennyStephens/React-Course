d9fe1e0216d2aea3da9453221467b08e30f59153
KennyStephens <kennethcstephens@gmail.com>
2018-07-20 22:28:17 -0600

    Used .setState to change the state.
    Changed the name of people in the Persons component by passing an argument in the switchNameHandler function. Used the switchNameHandler function as a property and changed the argument using an arrow function and bind(). Bind() is the preferred way of doing this because an arrow function is less efficient. Also did some styling, both on a separate stylesheet that was imported and with inline styling.

--- src/App.js
+++ src/App.js
@@ -11,15 +11,58 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked');
+    // Don't do this:this.state.persons[0].name = 'Maximilian';
+      this.setState({    
+        persons: [
+        {name: newName, age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+      ],
+      otherState: 'some other value'
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({    
+      persons: [
+      {name: 'Max', age: 28},
+      {name: event.target.value, age: 29},
+      {name: 'Stephanie', age: 26}
+    ],
+    otherState: 'some other value'
+  })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      paddding: '8px',
+      borderRadius: '4px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Max!')} 
+        changed={this.nameChangedHandler} >My Hobbies: Racing</Person> 
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
@@ -27,3 +70,7 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+/* Use bind() if you can instead of line 32 arrow function */
