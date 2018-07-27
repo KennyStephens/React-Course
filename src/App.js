53fda6e300009efa2f6605ada91fd221f9d0d82c
KennyStephens <kennethcstephens@gmail.com>
2018-07-27 16:36:34 -0600

    Continued learning how to style app. Used radium to do :hover and media-queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
+
 
 class App extends Component {
   state = {
@@ -65,7 +67,11 @@ class App extends Component {
       paddding: '8px',
       borderRadius: '4px',
       cursor: 'pointer',
-      padding: '5px 10px'
+      padding: '5px 10px',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -85,23 +91,38 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }  
+    if (this.state.persons.length <=1) {
+      classes.push('bold'); // classes=['red','bold]
     }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!!!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
+      </StyleRoot>
     );
+    
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
-export default App;
+export default Radium(App);
 
 
 
